ccd6e926deebd4d7171cc3aab2360d7592b123bd
Ricmal08 <malmgren.kontakt@gmail.com>
2025-09-16 15:52:41 +0200

    added initial structure to documents/doc view

--- src/views/doc.jsx
+++ src/views/doc.jsx
@@ -1,2 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
+
+function Doc({ isNew }) {
+    return (
+        <form>
+
+            <h2>{isNew ? 'Skapa Nytt Dokument' : 'Redigera Dokument'}</h2>
+
+            <div>
+                <label htmlFor="title">Titel:</label>
+                <input type="text" id="title" name="title" />
+            </div>
+
+
+            <div>
+                <label htmlFor="content">Innehåll:</label>
+                <textarea id="content" name="content" />
+            </div>
+
+            <button type="submit">Spara</button>
+        </form>
+    );
+}
+
+export default Doc;
