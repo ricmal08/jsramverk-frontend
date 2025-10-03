b65e2120dfb421745b5f40a88bbdbab3d1551643
Ricmal08 <malmgren.kontakt@gmail.com>
2025-10-03 13:30:41 +0200

    feat: initial setup for making Doc component controlled and implement fetch data

--- src/views/doc.jsx
+++ src/views/doc.jsx
@@ -1,7 +1,32 @@
+import { io } from "socket.io-client";
 import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 
+const socket = io("https://jsramverk-editor-jahl24-bfeufbb0dwcfg6a6.northeurope-01.azurewebsites.net/");//ändrar denna till lokal adress för test
+
 function Doc({ isNew }) {
+
+    //hämtar routerhooks
+    const { id } = useParams();
+    const navigate = useNavigate();
+
+    const [title, setTitle] = useState('');//tar nya värdet för Titel och triggar en omrendering av komponenten.
+    const [content, setContent] = useState('');//samma för innehållet i formuläret
+
+
+    useEffect(() => {
+        // Fetch som bara körs första gången och om vi ska redigera ett befintligt dokument
+        if (!isNew && id) {
+            fetch(`${apiUrl}documents/${id}`)//ev skruva på detta?
+                .then(response => response.json())
+                .then(data => {
+                    setTitle(data.title);
+                    setContent(data.content);
+                });
+        }
+    }, [id, isNew, apiUrl]);
+
+
     return (
         <form>
 
