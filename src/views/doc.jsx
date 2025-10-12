9fd38ae00805b886ba48cb164ad99d5d3ecf5a6f
Jakob <jahl24@student.bth.se>
2025-10-13 00:49:08 +0200

    Added submit handler for the form, enabling saving to backend

--- src/views/doc.jsx
+++ src/views/doc.jsx
@@ -4,7 +4,7 @@ import { useParams, useNavigate } from 'react-router-dom';
 function Doc({ isNew, apiUrl }) {
 
     // Hämta id från params
-    const { id } = useParams();
+    const { id } = useParams()
 
     // sätt title och content
     const [title, setTitle] = useState('')
@@ -22,9 +22,9 @@ function Doc({ isNew, apiUrl }) {
             })
             .then(response => {
                 if (!response.ok) {
-                    throw new Error('Nätverksfel');
+                    throw new Error('Nätverksfel')
                 }
-                return response.json();
+                return response.json()
             })
             .then(result => {
                 console.log('data retunerad:', result)
@@ -32,28 +32,60 @@ function Doc({ isNew, apiUrl }) {
                 setContent(result.data.document.content)
                 })
                 .catch(error => {
-                console.error('Error! Det gick inte att hämta dokument:', error);
+                console.error('Error! Det gick inte att hämta dokument:', error)
+                alert('Kunde inte hämta dokument')
             })
         }
     }, [isNew, id, apiUrl])
 
-    // Här ska dokuments sparas och muteras.
+    // Navigera efter submittad form
+    const navigate = useNavigate()
+
+    // Hantera submit
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        // Kolla om det är ett nytt dokument eller ej (post/put)
+        const method = isNew ? 'POST' : 'PUT'
+
+        // Olika url
+        const url = isNew ? `${apiUrl}create` : `${apiUrl}${id}`
+
+        fetch(url, {
+            method: method,
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ title, content })
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Kunde inte spara dokumentet');
+            }
+            navigate('/')
+        })
+        .catch(error => {
+            console.error('Error', error)
+            alert('Något gick fel vid sparning')
+        })
+    }
 
 
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
 
             <h2>{isNew ? 'Skapa Nytt Dokument' : 'Redigera Dokument'}</h2>
 
             <div>
                 <label htmlFor="title">Titel:</label>
-                <input type="text" id="title" name="title" value={title} />
+                <input type="text" id="title" name="title" required value={title} onChange={(e) => 
+                    setTitle(e.target.value)
+                } />
             </div>
 
 
             <div>
                 <label htmlFor="content">Innehåll:</label>
-                <textarea id="content" name="content" value={content} />
+                <textarea id="content" name="content" value={content} onChange={(e) => 
+                    setContent(e.target.value)} />
             </div>
 
             <button type="submit">Spara Dokument</button>
