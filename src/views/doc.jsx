35f20c7f526eb3220c2c7dc7386b2706421c37ad
Jakob <jahl24@student.bth.se>
2025-10-12 21:48:19 +0200

    Add fetching single document with graphQL

--- src/views/doc.jsx
+++ src/views/doc.jsx
@@ -1,7 +1,45 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 
-function Doc({ isNew }) {
+function Doc({ isNew, apiUrl }) {
+
+    // Hämta id från params
+    const { id } = useParams();
+
+    // sätt title och content
+    const [title, setTitle] = useState('')
+    const [content, setContent] = useState('')
+
+    useEffect(() => {
+        if (!isNew) {
+            fetch(`${apiUrl}graphql`, {
+                method:'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json',
+                },
+                body: JSON.stringify({ query: `{ document(id: "${id}") { _id title content } }` })
+            })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Nätverksfel');
+                }
+                return response.json();
+            })
+            .then(result => {
+                console.log('data retunerad:', result)
+                setTitle(result.data.document.title)
+                setContent(result.data.document.content)
+                })
+                .catch(error => {
+                console.error('Error! Det gick inte att hämta dokument:', error);
+            })
+        }
+    }, [isNew, id, apiUrl])
+
+    // Här ska dokuments sparas och muteras.
+
+
     return (
         <form>
 
@@ -9,13 +47,13 @@ function Doc({ isNew }) {
 
             <div>
                 <label htmlFor="title">Titel:</label>
-                <input type="text" id="title" name="title" />
+                <input type="text" id="title" name="title" value={title} />
             </div>
 
 
             <div>
                 <label htmlFor="content">Innehåll:</label>
-                <textarea id="content" name="content" />
+                <textarea id="content" name="content" value={content} />
             </div>
 
             <button type="submit">Spara Dokument</button>
