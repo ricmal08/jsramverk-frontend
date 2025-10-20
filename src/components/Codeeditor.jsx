72875384e9e8b54283216a582a166a21204be7af
Ricmal08 <malmgren.kontakt@gmail.com>
2025-10-20 12:11:07 +0200

    Fix: focus editor on Mount

--- src/components/Codeeditor.jsx
+++ src/components/Codeeditor.jsx
@@ -1,17 +1,27 @@
 import React from 'react';
-import {useState} from "react"
-import Editor from '@monaco-editor/react';
+import {useRef, useState} from "react"
+import {Editor} from '@monaco-editor/react';
 import { Box} from "@chakra-ui/react"
 
 const CodeEditor = () => {
-        const [value, setValue] = useState('//some comment')
+        const editorRef = useRef()
+    const [value, setValue] = useState("")
+
+    const onMount = (editor) => {
+        editorRef.current = editor
+        editor.focus()
+}
+
     return (
     <Box>
         <Editor
-        height="90vh"
+        height="75vh"
         theme="vs-dark"
         defaultLanguage="javascript"
         //defaultValue="//test kommentar"
+        onMount ={
+        onMount
+        }
         value={value}
         onChange={(value) => setValue(value)}
         />
