7f358643870c6aa69976e525a2f983482d53a50c
Ricmal08 <malmgren.kontakt@gmail.com>
2025-10-21 22:25:46 +0200

    feature: added Codeeditor and Codeview component and initial setup for integration

--- src/components/Codeeditor.jsx
+++ src/components/Codeeditor.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import {useRef, useState} from "react"
 import {Editor} from '@monaco-editor/react';
-import { Box} from "@chakra-ui/react"
+import { Box, HStack} from "@chakra-ui/react"
 import LanguageSelector from './Languageselector';
+import CodeView from './Codeview';
 
 const CodeEditor = () => {
         const editorRef = useRef()
@@ -18,21 +19,28 @@ const CodeEditor = () => {
         setLanguage(language)
     }
     return (
-    <Box position="relative" zIndex={10}>
-        <LanguageSelector language={language} onSelect={onSelect}/>
-        <Box position="relative" zIndex={1}>
-            <Editor
-            height="90vh"
-            theme="vs-dark"
-            language={language}
-            //defaultValue="//test kommentar"
-            onMount ={
-            onMount
-            }
-            value={value}
-            onChange={(value) => setValue(value)}
-            />
-        </Box>
+    <Box position="relative" zIndex={10}>{/*adderar zIndex för att lösa problemet med layers*/}
+        <HStack spacing={4}>
+            <Box w="50%">
+                <LanguageSelector language={language} onSelect={onSelect}/>
+                <Box position="relative" zIndex={1}>
+                    <Editor
+                    height="90vh"
+                    theme="vs-dark"
+                    language={language}
+                    //defaultValue="//test kommentar"
+                    onMount ={
+                    onMount
+                    }
+                    value={value}
+                    onChange={(value) => setValue(value)}
+                    />
+                </Box>
+            </Box>
+            <CodeView editorRef={editorRef} language={language}/>
+
+        </HStack>
+
     </Box>
     );
 }
